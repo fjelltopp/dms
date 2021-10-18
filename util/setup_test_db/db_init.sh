985c3d8e4c82e8b2d4a50b4a7ee77a8f8a47f113
manoj-nathwani <manoj_1993@hotmail.com>
2021-10-18 09:41:51 +0000

    enabled tests

--- util/setup_test_db/db_init.sh
+++ util/setup_test_db/db_init.sh
@@ -1,3 +1,3 @@
-docker run --name dms-postgres -e POSTGRES_PASSWORD=pass -p 5432:5432 -d postgres
-cat db_init.sql | docker exec -i dms-postgres psql -U postgres
-ckan -c ../../ckan/test-core.ini datastore set-permissions | docker exec -i dms-postgres psql -U postgres
+docker-compose up -d
+cat db_init.sql | docker exec -i db psql -U ckan
+ckan -c ../../ckan/test-core.ini datastore set-permissions | docker exec -i db psql -U ckan
